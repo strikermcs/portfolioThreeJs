103232c38ec0325ea8ab9616f26ca6b796932ef2
Павел Семенюк <tellermcs@gmail.com>
2023-03-16 11:34:53 +0200

    make project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, 
+         Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
+const App = () => {
+  return (
+    <>
+     Hello Portfolio
+    </>
+  )
+}
+
+export default App
