560c9a90705e1d550632fa028dab4f349bd9a027
Павел Семенюк <tellermcs@gmail.com>
2023-03-29 20:36:50 +0300

    add main section and about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import { About, Contact, Experience, Feedbacks,
 
 const App = () => {
   return (
-    <>
-     Hello Portfolio
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
